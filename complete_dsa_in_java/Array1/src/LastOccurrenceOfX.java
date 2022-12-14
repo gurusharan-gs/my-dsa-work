5910842e8fa3fd1c6dbeaffd8b80e0bdbbcf3217
Gurusharan <gurusharanulagadde143@gmail.com>
2022-12-14 18:15:06 +0530

    up

--- complete_dsa_in_java/Array1/src/LastOccurrenceOfX.java
+++ complete_dsa_in_java/Array1/src/LastOccurrenceOfX.java
@@ -16,10 +16,24 @@ public class LastOccurrenceOfX {
 	public static void main(String[] args) {
 		
 		int[] arr = {5,2,3,5,5,7,2,5};
-		int x = 5;
-		int res = lastOccurrenceOfX(arr, x);
+		int target = 5;
+		int res = findlastOccur(arr, target);
 		System.out.println(res);
 
+	}
+	
+	
+	static int findlastOccur(int[] arr, int target) {
+		
+		int res = -1;
+		for(int i=0; i<arr.length; i++) {
+			if(arr[i] == target) {
+				res = i;
+			
+			}
+		}
+		return res;
+		
 	}
 
 
