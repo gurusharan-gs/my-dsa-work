eec0c21c820996f7a306ae4671986ac82e92212f
Gurusharan <gurusharanulagadde143@gmail.com>
2023-01-08 11:03:05 +0530

    bubble

---
+++ complete_dsa_in_java/Array1/src/sorting/Bubble.java
@@ -0,0 +1,36 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class Bubble {
+
+	static void bubble(int[] arr) {
+		
+		boolean flag;
+		
+		for(int i = 0; i < arr.length; i++) {
+			flag = false;
+			for(int j = 1; j < arr.length - i; j++) {
+				if (arr[j] < arr[j - 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j - 1];
+					arr[j - 1] = temp;
+					flag = true;
+				}
+			}
+			if(!flag) {
+				break;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] arr = { 5, 4, 2, 3, 1 };
+		bubble(arr);
+		System.out.println(Arrays.toString(arr));
+
+	}
+
+}
