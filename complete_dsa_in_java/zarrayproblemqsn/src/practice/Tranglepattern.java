10c311bc862e548d26f246b326a2af5e0096f0ef
Gurusharan <gurusharanulagadde143@gmail.com>
2023-03-10 18:38:59 +0530

    d

--- complete_dsa_in_java/zarrayproblemqsn/src/practice/Tranglepattern.java
+++ complete_dsa_in_java/zarrayproblemqsn/src/practice/Tranglepattern.java
@@ -1,16 +1,38 @@
 package practice;
 
 public class Tranglepattern {
-	
+
 	public static void main(String[] args) {
-		
-		for(int i=1; i<=4; i++) {
-			for(int j=1; j<=i; j++) {
+
+		for (int i = 1; i <= 4; i++) {
+			for (int j = 1; j <= i; j++) {
 				System.out.print(j);
 			}
 			System.out.println();
 		}
-		
+
+		System.out.println("====================");
+
+		for (int i = 4; i >= 0; i--) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+
+		System.out.println("====================");
+
+		for (int i = 1; i <= 4; i++) {
+			for (int j = 1; j <= 4; j++) {
+				if(i==1 || i==4 || j==1 || j==4) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+
 	}
 
 }
