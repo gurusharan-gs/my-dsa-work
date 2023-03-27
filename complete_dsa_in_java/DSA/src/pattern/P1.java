847ea6a3d4d597f0bcd836937be885f2f017e5de
Gurusharan <gurusharanulagadde143@gmail.com>
2023-03-27 19:56:47 +0530

    done

---
+++ complete_dsa_in_java/DSA/src/pattern/P1.java
@@ -0,0 +1,66 @@
+package pattern;
+
+public class P1 {
+
+//	pattern 1
+	public static void pattern1(int n) {
+
+		for (int row = 1; row <= n; row++) {
+			for (int col = 1; col <= n; col++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+
+	}
+
+//	pattern 2
+	public static void pattern2(int n) {
+
+//		this is outside loop 
+		for (int row = 1; row <= n; row++) {
+			for (int col = 1; col <= row; col++) {
+//				this is printing star
+				System.out.print("* ");
+			}
+//			this is a the print the next line
+			System.out.println();
+		}
+
+	}
+	
+//	pattern 3
+	public static void pattern3(int n) {
+
+		for (int row = 1; row <= n; row++) {
+			for (int col = row; col <= n; col++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+
+	}
+	
+//	pattern 3
+	public static void pattern4(int n) {
+
+		for (int row = 1; row <= n; row++) {
+			for (int col = 1; col <= n-row+1; col++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+
+	}
+	
+	
+
+	public static void main(String[] args) {
+
+		pattern3(4);
+		System.out.println("-------------------------------");
+		pattern4(4);
+
+	}
+
+}
--- complete_dsa_in_java/productApp/src/main/java/com/masai/model/Category.java
+++ complete_dsa_in_java/productApp/src/main/java/com/masai/model/Category.java
@@ -2,6 +2,7 @@ package com.masai.model;
 
 import java.util.List;
 
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
