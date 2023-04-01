d95828db69245b7207ec22f6513a621bb4d43a49
Gurusharan Ulagadde <85774667+gurusharan-gs@users.noreply.github.com>
2023-04-01 20:07:33 +0530

    Update ArrayIsSorted.java

--- complete_dsa_in_java/Array1/src/ArrayIsSorted.java
+++ complete_dsa_in_java/Array1/src/ArrayIsSorted.java
@@ -13,12 +13,21 @@ public class ArrayIsSorted {
 		return cheak;
 		
 	}
+	
+ public static void arrayIsSorted1(int[] arr) {
+		
+           List<Integer> list = new Arrays.asList(1,5,4,2,3,6,4,8);
+	 list.stream().sorted().forEach(s -> System.out.print(s +" ");
+		
+	}
 
 	public static void main(String[] args) {
 		int[] arr = {5,2,3,5,5,7,2,5};
 	
 		boolean res = arrayIsSorted(arr);
 		System.out.println(res);
+		
+		arrayIsSorted1();
 
 	}
 
